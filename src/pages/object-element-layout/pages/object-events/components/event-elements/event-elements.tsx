69cc8294521642f2bbd8472f23e293baeb05a829
Danilok95432 <danila.dvoretskov@yandex.ru>
2024-12-02 15:07:36 +0300

    События объекта

---
+++ src/pages/object-element-layout/pages/object-events/components/event-elements/event-elements.tsx
@@ -0,0 +1,125 @@
+import { type ObjectEvents } from 'src/types/objects'
+import { useParams } from 'react-router-dom'
+import { type FC } from 'react'
+import cn from 'classnames'
+
+import { mainFormatDate } from 'src/helpers/utils'
+import {
+	useDeleteObjectEventsByIdMutation,
+	useGetEventsByObjectIdQuery,
+} from 'src/store/objects/objects.api'
+import { useTableSearch } from 'src/hooks/table-search/table-search'
+
+import { CustomTable } from 'src/components/custom-table/custom-table'
+import { Loader } from 'src/components/loader/loader'
+import { RowController } from 'src/components/row-controller/row-controller'
+import { GridRow } from 'src/components/grid-row/grid-row'
+import { TableSearchInput } from 'src/modules/table-search-input/table-search'
+
+import styles from './index.module.scss'
+
+export const EventElements: FC = () => {
+	const { id } = useParams()
+
+	const { handleSearch, searchParams } = useTableSearch([
+		'title',
+		'typeEvent',
+		'typePart',
+		'startDate',
+		'endDate',
+	])
+	const { data: events, isLoading } = useGetEventsByObjectIdQuery({
+		id,
+		search: searchParams.title,
+	})
+
+	const [deleteNewsById] = useDeleteObjectEventsByIdMutation()
+
+	const tableTitles = ['Наименование', 'Тип события', 'Тип участия', 'Начало', 'Окончание', '']
+	const formatObjectsTableData = (eventsData: ObjectEvents[]) => {
+		return eventsData.map((eventsEl) => {
+			return {
+				rowId: eventsEl.id,
+				cells: [
+					<p className={cn({ 'hidden-cell-icon': eventsEl.isHidden })} key='0'>
+						{eventsEl.title}
+					</p>,
+					<p className={cn({ 'hidden-cell': eventsEl.isHidden })} key='1'>
+						{eventsEl.typeEvent}
+					</p>,
+					<p className={cn({ 'hidden-cell': eventsEl.isHidden })} key='2'>
+						{eventsEl.typePart}
+					</p>,
+					<p className={cn({ 'hidden-cell': eventsEl.isHidden })} key='3'>
+						{mainFormatDate(eventsEl.startDate)}
+					</p>,
+					<p className={cn({ 'hidden-cell': eventsEl.isHidden })} key='4'>
+						{mainFormatDate(eventsEl.endDate)}
+					</p>,
+					<RowController
+						id={eventsEl.id}
+						hideHandler={rowHideHandler}
+						removeHandler={rowDeleteHandler}
+						textOfHidden='Скрыть направление'
+						key='5'
+					/>,
+				],
+			}
+		})
+	}
+
+	const rowDeleteHandler = async (eventId: string) => {
+		await deleteNewsById({ objectId: id ?? '', eventId })
+	}
+
+	const rowHideHandler = async (id: string) => {
+		console.log(id + 'спрятан')
+	}
+
+	if (isLoading || !events) return <Loader />
+
+	return (
+		<div>
+			<GridRow
+				$margin='0 0 15px 0'
+				$padding='0 29px'
+				$template='auto /   minmax(300px, 2.2fr)
+        minmax(130px, 1.1fr)
+        minmax(130px, 1.3fr)
+        minmax(100px, 0.6fr)
+        minmax(120px, 0.6fr)
+        minmax(50px, 0.2fr)'
+			>
+				<TableSearchInput
+					handleSearch={(val) => handleSearch('title', val)}
+					placeholder='искать по наименованию события'
+				/>
+				<TableSearchInput
+					handleSearch={(val) => handleSearch('typeEvent', val)}
+					placeholder='тип события'
+				/>
+				<TableSearchInput
+					handleSearch={(val) => handleSearch('typePart', val)}
+					placeholder='тип участия'
+				/>
+				<TableSearchInput
+					handleSearch={(val) => handleSearch('startDate', val)}
+					placeholder='начало'
+					$variant='date'
+					mask={Date}
+				/>
+				<TableSearchInput
+					handleSearch={(val) => handleSearch('endDate', val)}
+					placeholder='окончание'
+					$variant='date'
+					mask={Date}
+				/>
+			</GridRow>
+			<CustomTable
+				className={styles.eventsTable}
+				rowData={formatObjectsTableData(events)}
+				colTitles={tableTitles}
+			/>
+		</div>
+	)
+}
--- src/store/objects/objects.api.ts
+++ src/store/objects/objects.api.ts
@@ -1,4 +1,4 @@
-import { type ObjectNews, type ObjectItem } from 'src/types/objects'
+import { type ObjectNews, type ObjectItem, type ObjectEvents } from 'src/types/objects'
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
@@ -6,7 +6,7 @@ import { BASE_URL, ReducerPath } from 'src/helpers/consts'
 
 export const objectsApi = createApi({
 	reducerPath: ReducerPath.Objects,
-	tagTypes: ['Object', 'ObjectNews'],
+	tagTypes: ['Object', 'ObjectNews', 'ObjectEvents'],
 	baseQuery: fetchBaseQuery({
 		baseUrl: BASE_URL,
 	}),
@@ -35,6 +35,15 @@ export const objectsApi = createApi({
 			}),
 			providesTags: ['Object', 'ObjectNews'],
 		}),
+		getEventsByObjectId: build.query<ObjectEvents[], { id: string | undefined; search?: string }>({
+			query: ({ id: objId, search }) => ({
+				url: `objects/${objId}/events`,
+				params: {
+					q: search,
+				},
+			}),
+			providesTags: ['Object', 'ObjectEvents'],
+		}),
 		deleteObjectById: build.mutation<null, string>({
 			query: (objectId) => ({
 				url: `objectDelete/${objectId}`,
@@ -49,6 +58,13 @@ export const objectsApi = createApi({
 			}),
 			invalidatesTags: ['Object', 'ObjectNews'],
 		}),
+		deleteObjectEventsById: build.mutation<null, { objectId: string; eventId: string }>({
+			query: ({ objectId, eventId }) => ({
+				url: `/object/${objectId}/eventDelete/${eventId}`,
+				method: 'DELETE',
+			}),
+			invalidatesTags: ['Object', 'ObjectEvents'],
+		}),
 	}),
 })
 
@@ -58,4 +74,6 @@ export const {
 	useDeleteObjectByIdMutation,
 	useGetNewsByObjectIdQuery,
 	useDeleteObjectNewsByIdMutation,
+	useGetEventsByObjectIdQuery,
+	useDeleteObjectEventsByIdMutation,
 } = objectsApi

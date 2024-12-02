e25d9e50c74b66627722a39014bbbae44e82293a
luspi1 <igbabin566@gmail.com>
2024-12-02 12:02:06 +0300

    правки объекта

--- src/types/objects.ts
+++ src/types/objects.ts
@@ -1,13 +1,4 @@
-import { type ObjectGalleryLink } from 'src/pages/object-element-layout/pages/object-gallery/schema'
 import { type FileWithPreview } from './files'
-
-export type ObjLink = {
-	id: string
-	title: string
-	link: string
-	date: string
-	source: string
-}
 
 export type ObjectNews = {
 	id: string
@@ -18,28 +9,23 @@ export type ObjectNews = {
 	date: Date
 }
 
-export type ObjectGallery = {
-	objectImages?: FileWithPreview[]
-	links?: ObjectGalleryLink[]
-}
-
 export type ObjectItem = {
 	id: string
 	isHidden: boolean
 	title: string
 	type: string
 	relation: string
-	objectMainImg?: FileWithPreview[]
+	objectMainImg: FileWithPreview[]
 	objectDesc: string
 	objectDescFull: string
 	phoneNumber?: string
 	emailAddress?: string
 	emailAddressFull: string
-	tgText?: string
+	tgText: string
 	tgAddress?: string
-	vkText?: string
+	vkText: string
 	vkAddress?: string
 	coords: string
 	news: ObjectNews[]
-	gallery: ObjectGallery
+	objectImages: FileWithPreview[]
 }

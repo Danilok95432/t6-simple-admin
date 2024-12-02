cbab806fb657dacf9014b65205cb2fd0594a81af
luspi1 <igbabin566@gmail.com>
2024-12-02 17:51:04 +0300

    правки ошибок при сборке

--- src/pages/one-event-layout/pages/event-timetable-layout/pages/admin-event-timetable-list/admin-event-timetable-list.tsx
+++ src/pages/one-event-layout/pages/event-timetable-layout/pages/admin-event-timetable-list/admin-event-timetable-list.tsx
@@ -3,7 +3,6 @@ import { type FC } from 'react'
 import { AdminContent } from 'src/components/admin-content/admin-content'
 import { EventTitle } from 'src/components/event-title/event-title'
 import { AdminButton } from 'src/UI/AdminButton/AdminButton'
-import { AdminRoute } from 'src/routes/admin-routes/consts'
 
 import adminStyles from 'src/routes/admin-layout/index.module.scss'
 import { FlexRow } from 'src/components/flex-row/flex-row'
@@ -19,12 +18,7 @@ export const AdminEventTimetableList: FC = () => {
 				address='с. Атманов Угол Тамбовской области'
 			/>
 			<FlexRow>
-				<AdminButton
-					className={adminStyles.adminViewPageLink}
-					as='link'
-					to={AdminRoute.AdminEventTimetableEdit}
-					$margin='0'
-				>
+				<AdminButton className={adminStyles.adminViewPageLink} as='link' $margin='0'>
 					Перейти к редактированию расписания
 				</AdminButton>
 			</FlexRow>

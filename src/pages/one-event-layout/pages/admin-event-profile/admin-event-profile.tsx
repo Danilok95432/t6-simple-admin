e111cad65d7a850740d664e5386d8181fcc29f98
luspi1 <igbabin566@gmail.com>
2024-12-03 18:12:49 +0300

    продолжение профиля события

--- src/pages/one-event-layout/pages/admin-event-profile/admin-event-profile.tsx
+++ src/pages/one-event-layout/pages/admin-event-profile/admin-event-profile.tsx
@@ -14,8 +14,6 @@ import { AdminRoute } from 'src/routes/admin-routes/consts'
 import { TitleSection } from 'src/pages/one-event-layout/pages/admin-event-profile/components/title-section/title-section'
 import { DateSection } from 'src/pages/one-event-layout/pages/admin-event-profile/components/date-section/date-section'
 import { DescSection } from 'src/pages/one-event-layout/pages/admin-event-profile/components/desc-section/desc-section'
-import { AreaSection } from 'src/pages/one-event-layout/pages/admin-event-profile/components/area-section/area-section'
-import { NewAreaSection } from 'src/pages/one-event-layout/pages/admin-event-profile/components/new-area-section/new-area-section'
 
 import adminStyles from 'src/routes/admin-layout/index.module.scss'
 import styles from './index.module.scss'
@@ -24,10 +22,6 @@ export const AdminEventProfile: FC = () => {
 	const methods = useForm<EventProfileInputs>({
 		mode: 'onBlur',
 		resolver: yupResolver(eventProfileSchema),
-		defaultValues: {
-			newAreaSection: false,
-			saveNewArea: true,
-		},
 	})
 
 	const onSubmit: SubmitHandler<EventProfileInputs> = (data) => {
@@ -35,11 +29,8 @@ export const AdminEventProfile: FC = () => {
 	}
 
 	return (
-		<AdminContent $backgroundColor='#ffffff'>
-			<Link
-				to={`/${AdminRoute.AdminAtmans}/${AdminRoute.AdminEventsList}`}
-				className={adminStyles.adminReturnLink}
-			>
+		<AdminContent className={styles.eventProfilePage} $backgroundColor='#ffffff'>
+			<Link to={`/${AdminRoute.AdminEventsList}`} className={adminStyles.adminReturnLink}>
 				Возврат к списку событий
 			</Link>
 			<h3>Профиль события</h3>
@@ -53,8 +44,6 @@ export const AdminEventProfile: FC = () => {
 					<TitleSection />
 					<DateSection />
 					<DescSection />
-					<AreaSection />
-					<NewAreaSection />
 					<AdminControllers outLink={AdminRoute.AdminHome} variant='1' />
 				</form>
 			</FormProvider>
--- src/pages/one-event-layout/pages/admin-event-profile/schema.ts
+++ src/pages/one-event-layout/pages/admin-event-profile/schema.ts
@@ -2,9 +2,9 @@ import * as yup from 'yup'
 
 export type EventProfileInputs = {
 	eventName: string
+	eventFace?: string
 	eventType: string
 	eventLevel: string
-	eventSite?: string
 	eventTags?: string
 	dateEventStart: Date
 	timeEventStart: Date
@@ -13,19 +13,9 @@ export type EventProfileInputs = {
 	shortDesc: string
 	fullDesc: string
 	eventConditions: string
+	eventSchedule: string
 	visitingAge?: string
-	participationAge?: string
 	areaList: string
-	areaName: string
-	newAreaSection?: boolean
-	newAreaName?: string
-	newAreaLocation?: string
-	newAreaDesc?: string
-	newAreaAddress?: string
-	newAreaWidget?: string
-	newAreaType?: string
-	saveNewArea?: boolean
-	availableNewArea?: boolean
 }
 
 export const eventProfileSchema = yup.object().shape({
@@ -39,18 +29,6 @@ export const eventProfileSchema = yup.object().shape({
 	shortDesc: yup.string().required('Введите краткое описание'),
 	fullDesc: yup.string().required('Введите подробное описание'),
 	eventConditions: yup.string().required('Укажите условия'),
+	eventSchedule: yup.string().required('Укажите расписание'),
 	areaList: yup.string().required().notOneOf(['0'], 'Выберите значение'),
-	areaName: yup.string().required('Введите название'),
-	newAreaSection: yup.boolean(),
-	newAreaName: yup.string().when('newAreaSection', ([newAreaSection], schema) => {
-		return newAreaSection ? schema.required('Укажите название площадки') : schema.notRequired()
-	}),
-	newAreaLocation: yup.string().when('newAreaSection', ([newAreaSection], schema) => {
-		return newAreaSection ? schema.required('Укажите населенный пункт') : schema.notRequired()
-	}),
-	newAreaType: yup.string().when('newAreaSection', ([newAreaSection], schema) => {
-		return newAreaSection
-			? schema.required('Выберите тип').notOneOf(['0'], 'Выберите тип')
-			: schema.notRequired()
-	}),
 })

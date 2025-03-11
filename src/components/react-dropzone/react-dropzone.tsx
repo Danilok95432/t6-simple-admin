9173513c20e677258aa608683c8eeda956563694
Danilok95432 <danila.dvoretskov@yandexe.ru>
2025-03-11 17:04:14 +0300

    правки фото для элемента материальной культуры

--- src/components/react-dropzone/react-dropzone.tsx
+++ src/components/react-dropzone/react-dropzone.tsx
@@ -88,7 +88,7 @@ export const ReactDropzone: FC<ReactDropzoneProps> = ({
 				} else {
 					formData.append('itemimage', file)
 					formData.append('imgtype', imgtype)
-					formData.append('id', id)
+					formData.append('id_item', id)
 				}
 
 				const response = await uploadImages(formData).unwrap()
